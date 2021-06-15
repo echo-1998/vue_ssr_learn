b430caaa72c0507cbf4a17948789921a4991cb77
echo-1998 <77541903+echo-1998@users.noreply.github.com>
2021-06-15 22:57:30 +0800

    与服务器集成
    npm install express --save

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const server = require('express')()
 const Vue = require("vue")
 const renderer = require("vue-server-renderer").createRenderer()
-const app = new Vue({
-    template: `<div>aaa</div>`
-})
-renderer.renderToString(app, (err, html) => {
-    if (err) {
-        throw err
-    }
-    console.log(html)
+server.get('*', (req, res) => {
+    const app = new Vue({
+        template: `<div>aaa</div>`
+    })
+    renderer.renderToString(app, (err, html) => {
+        if (err) {
+            throw err
+        }
+        console.log(html)
+    })
 })
 
 

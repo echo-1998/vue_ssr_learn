90ee27ffb5888731f45955bdbe5a2067e461e993
echo-1998 <77541903+echo-1998@users.noreply.github.com>
2021-06-15 22:58:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+const Vue = require('vue')
 const server = require('express')()
-const Vue = require("vue")
-const renderer = require("vue-server-renderer").createRenderer()
+const renderer = require('vue-server-renderer').createRenderer()
+
 server.get('*', (req, res) => {
-    const app = new Vue({
-        template: `<div>aaa</div>`
-    })
-    renderer.renderToString(app, (err, html) => {
-        if (err) {
-            throw err
-        }
-        console.log(html)
-    })
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `)
+  })
 })
 
-
+server.listen(8080)
